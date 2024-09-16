6bbf228db63f64ee732773fea94f4dac34590b28
Elena Hanna <hanna_elena@live.se>
2024-09-16 13:04:59 +0000

    Add trusted origin for csrf protection

--- pixelpals_backend/settings.py
+++ pixelpals_backend/settings.py
@@ -95,6 +95,12 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-enzolita-pixelpalsbacke-fagxslbmqhy.ws.codeinstitute-ide.net',
+]
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
