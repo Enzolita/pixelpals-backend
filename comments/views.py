d6261801452a63d7abaa56cb50c67ad07718ec2c
Elena Hanna <hanna_elena@live.se>
2024-09-18 13:07:37 +0000

    Add serializer for the like model

--- comments/views.py
+++ comments/views.py
@@ -4,6 +4,7 @@ from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
 
+
 class CommentList(generics.ListCreateAPIView):
     """
     List comments or create a comment if logged in.
